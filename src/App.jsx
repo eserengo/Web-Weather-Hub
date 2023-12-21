1648301693864af04082e31ee9bb567b8ddf3e66
eserengo <eserengo@gmail.com>
2023-12-21 08:01:19 -0300

    Added base files: Vite, React, Bootstrap, Sass,assets - first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.scss'
+
+const App = () => {
+
+  return (
+    <h1 className='fs-2 fw-normal text-offBlack text-xs-center text-lg-end'>Hello</h1>
+  )
+}
+
+export default App;
